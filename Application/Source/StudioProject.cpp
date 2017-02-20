80ebdf6e97ee4f4caa7ddc8c4a2ff2e259b738e0
unknown <162670T@M314G101.sidm.student.local>
2017-02-20 15:46:02 +0800

    >fix

--- Application/Source/StudioProject.cpp
+++ Application/Source/StudioProject.cpp
@@ -511,7 +511,7 @@ void StudioProject::Render()
 	for (int i = 0; i < 20; i++)
 	{
 		if (i == 20/2)
-			RenderTextOnScreen(meshList[GEO_TEXT], "[" + std::to_string(PlayerBase::instance()->getPlayerLevel()) + "]", Color(1, 1, 0), 2.5, 15.3, 2);
+			RenderTextOnScreen(meshList[GEO_TEXT], "[" + std::to_string(PlayerBase::instance()->getPlayerLevel()) + "]", Color(1, 1, 0), 2.5, 15.1, 2);
 		if (i < ((double)((((double)PlayerBase::instance()->getPlayerExperience()) / ((double)PlayerBase::instance()->getPlayerLevelCap())) * 100) / 5))
 			experienceBar += "O";  
 		else
