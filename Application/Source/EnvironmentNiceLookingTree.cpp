e2386512772842f6a7a2e1828839c68e2bb6b9fa
unknown <160283Z@M314G103.sidm.student.local>
2017-02-23 14:43:27 +0800

    >NPCs and some nonsense

--- Application/Source/EnvironmentNiceLookingTree.cpp
+++ Application/Source/EnvironmentNiceLookingTree.cpp
@@ -13,18 +13,63 @@ public:
 		boundry_[0][0] = '#';
 		boundry_[0][1] = '#';
 		boundry_[0][2] = '#';
+		boundry_[0][3] = '#';
+		boundry_[0][4] = '#';
+		boundry_[0][5] = '#';
+		boundry_[0][6] = '#';
+
 
 		boundry_[1][0] = '#';
-		boundry_[1][1] = 'N';
+		boundry_[1][1] = '#';
+		boundry_[1][1] = '#';
 		boundry_[1][2] = '#';
+		boundry_[1][3] = '#';
+		boundry_[1][4] = '#';
+		boundry_[1][5] = '#';
+		boundry_[1][6] = '#';
+
 
 		boundry_[2][0] = '#';
 		boundry_[2][1] = '#';
 		boundry_[2][2] = '#';
 		boundry_[2][3] = '#';
 		boundry_[2][4] = '#';
+		boundry_[2][5] = '#';
+		boundry_[2][6] = '#';
 
+
+		boundry_[3][0] = '#';
+		boundry_[3][1] = '#';
 		boundry_[3][2] = '#';
+		boundry_[3][3] = 'N';
+		boundry_[3][4] = '#';
+		boundry_[3][5] = '#';
+		boundry_[3][6] = '#';
+
+
+		boundry_[4][0] = '#';
+		boundry_[4][1] = '#';
+		boundry_[4][2] = '#';
+		boundry_[4][3] = '#';
+		boundry_[4][4] = '#';
+		boundry_[4][5] = '#';
+		boundry_[4][6] = '#';
+
+		boundry_[5][0] = '#';
+		boundry_[5][1] = '#';
+		boundry_[5][2] = '#';
+		boundry_[5][3] = '#';
+		boundry_[5][4] = '#';
+		boundry_[5][5] = '#';
+		boundry_[5][6] = '#';
+
+		boundry_[6][0] = '#';
+		boundry_[6][1] = '#';
+		boundry_[6][2] = '#';
+		boundry_[6][3] = '#';
+		boundry_[6][4] = '#';
+		boundry_[6][5] = '#';
+		boundry_[6][6] = '#';
 
 	}
 
